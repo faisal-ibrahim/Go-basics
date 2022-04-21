510eb26b8b514ce6f82b09700601404bbefe2dd3
Faisal Ibrahim <faisal.im048@gmail.com>
2022-04-21 23:12:07 +0200

    Iterating a Linked List in Go

--- linked-list-in-go.go
+++ linked-list-in-go.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	fmt.Println("Go Linked Lists Tutorial")
+
 	mylist := list.New()
 	mylist.PushBack(1)
 	mylist.PushFront(2)
-	// we now have a linked list with '1' at the back of the list
-	// and '2' at the front of the list.
+
+	for element := mylist.Front(); element != nil; element = element.Next() {
+		// do something with element.Value
+		fmt.Println(element.Value)
+	}
+
 }
