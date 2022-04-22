c91820891000d3b892023c6e70f7b332ea1219b1
Faisal Ibrahim <faisal.im048@gmail.com>
2022-04-22 23:42:25 +0200

    Removing Elements from a Linked List

--- linked-list-in-go.go
+++ linked-list-in-go.go
@@ -6,11 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Go Linked Lists Tutorial")
 
 	mylist := list.New()
 	mylist.PushBack(1)
 	mylist.PushFront(2)
+
+	for element := mylist.Front(); element != nil; element = element.Next() {
+		// do something with element.Value
+		if element.Value != 1 {
+			mylist.Remove(element)
+		}
+	}
 
 	for element := mylist.Front(); element != nil; element = element.Next() {
 		// do something with element.Value
